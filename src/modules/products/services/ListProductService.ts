24f1b9d9870965f5608a65d965c8bc9242049fc2
Vitor Oliveira <vitoro.fabiano@gmail.com>
2025-06-11 15:50:37 -0300

    feat: add CreateProductService, ListProductService, and ShowProductService for product management

---
+++ src/modules/products/services/ListProductService.ts
@@ -0,0 +1,10 @@
+import { Product } from "../database/entities/Product";
+import { productsRepositories } from "../database/repositories/ProductsRepositories";
+
+export default class ListProductService {
+  async execute(): Promise<Product[]> {
+    const products = await productsRepositories.find();
+
+    return products;
+  }
+}
