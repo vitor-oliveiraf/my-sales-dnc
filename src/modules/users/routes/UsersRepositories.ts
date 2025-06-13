bd745e7507bb16551bcebd5e2e5575507dcfb38b
Vitor Oliveira <vitoro.fabiano@gmail.com>
2025-06-13 10:05:12 -0300

    feat: create UsersRepositories for user data access with methods to find users by name, email, and ID

---
+++ src/modules/users/routes/UsersRepositories.ts
@@ -0,0 +1,14 @@
+import { AppDataSource } from "../../../shared/typeorm/data-source";
+import { User } from "../database/entities/User";
+
+export const usersRepository = AppDataSource.getRepository(User).extend({
+  async findByName(name: string): Promise<User | null> {
+    return this.findOneBy({ name });
+  },
+  async findByEmail(email: string): Promise<User | null> {
+    return this.findOneBy({ email });
+  },
+  async findById(id: number): Promise<User | null> {
+    return this.findOneBy({ id });
+  },
+});
