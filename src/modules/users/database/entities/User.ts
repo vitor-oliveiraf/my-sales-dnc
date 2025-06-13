72d851a6a54e16c2b76b0dfea54241e9bd17a331
Vitor Oliveira <vitoro.fabiano@gmail.com>
2025-06-13 09:53:31 -0300

    feat: implement User entity with fields for id, name, email, password, avatar, and timestamps

---
+++ src/modules/users/database/entities/User.ts
@@ -0,0 +1,31 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from "typeorm";
+
+@Entity("users")
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  avatar: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
